e09d165264e7e0ff0c6abd7498f0d766dea393cb
K0H205 <hrswkhd@gmail.com>
2018-08-25 18:51:21 +0900

    ルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 Rails.application.routes.draw do
-  get 'tasks/new'
-  get 'tasks/index'
-  get 'tasks/create'
-  get 'tasks/index'
-  get 'tasks/update'
-  get 'tasks/destroy'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :tasks
+  
 end
