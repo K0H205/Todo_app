976ed76741e1a393f37d0016d5ff9f13454203e9
K0H205 <hrswkhd@gmail.com>
2018-08-25 18:37:25 +0900

    doneカラムの初期値を設定

---
+++ db/migrate/20180825064906_changedonecolumn_totasks.rb
@@ -0,0 +1,5 @@
+class ChangedonecolumnTotasks < ActiveRecord::Migration[5.2]
+  def change
+    change_column :tasks, :done, :boolean, :default => false
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_25_063248) do
+ActiveRecord::Schema.define(version: 2018_08_25_064906) do
 
   create_table "tasks", force: :cascade do |t|
     t.string "content"
-    t.boolean "done"
+    t.boolean "done", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
