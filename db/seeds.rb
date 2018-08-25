4078dd9c38088da1f2fc73eda6cdc01512a9351b
K0H205 <hrswkhd@gmail.com>
2018-08-25 18:41:41 +0900

    サンプルデータ投入

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Task.create!([
-    { content: "HEllo!" },
-  ])
+10.times do |i|
+    Task.create(content: "task #{i}")
+end
