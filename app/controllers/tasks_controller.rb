785cad584f2f27041b19ae61dddb43a299a9f33f
K0H205 <hrswkhd@gmail.com>
2018-08-26 16:02:39 +0900

    APIcontrollerのbefore_action修正

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,9 @@
 class TasksController < ApplicationController
   
-  before_action :set_task, only: [:show, :update, :destroy]
+  before_action :set_task, only: [:update, :destroy]
 
   def index
     @tasks = Task.all
-
     render json: @tasks
   end
 
